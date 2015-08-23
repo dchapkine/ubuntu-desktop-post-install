8d975213f28cdc7fb35b2d9d907449a20d3cb503
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-08-23 19:57:33 +0200

    update virtualbox install

--- install_virtualbox.sh
+++ install_virtualbox.sh
@@ -10,9 +10,9 @@ echo "running "$SCRIPT_NAME
 # tested: works on ubuntu 14.04.3 gnome
 # http://doc.ubuntu-fr.org/virtualbox
 
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -  >> $SCRIPT_LOGFILE
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -  &>> $SCRIPT_LOGFILE
 echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
-sudo apt-get update >> $SCRIPT_LOGFILE
-sudo apt-get -y install virtualbox-5.0 >> $SCRIPT_LOGFILE
-sudo usermod -G vboxusers -a $USER
+sudo apt-get update &>> $SCRIPT_LOGFILE
+sudo apt-get -y install virtualbox-5.0 &>> $SCRIPT_LOGFILE
+sudo usermod -G vboxusers -a $USER &>> $SCRIPT_LOGFILE
 
