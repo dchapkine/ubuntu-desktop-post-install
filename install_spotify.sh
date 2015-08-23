c4fc4c92d77c8b5b243db317a4d510facf10083d
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-08-23 20:01:26 +0200

    update spotity install

--- install_spotify.sh
+++ install_spotify.sh
@@ -11,13 +11,13 @@ echo "running "$SCRIPT_NAME
 # https://www.spotify.com/fr/download/linux/
 
 # 1. Add the Spotify repository signing key to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886 &>> $SCRIPT_LOGFILE
 
 # 2. Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list &>> $SCRIPT_LOGFILE
 
 # 3. Update list of available packages
-sudo apt-get -y update >> $SCRIPT_LOGFILE
+sudo apt-get -y update &>> $SCRIPT_LOGFILE
 
 # 4. Install Spotify
-sudo apt-get install -y spotify-client >> $SCRIPT_LOGFILE
+sudo apt-get install -y spotify-client &>> $SCRIPT_LOGFILE
