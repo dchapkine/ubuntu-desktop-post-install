3bd8e96d303f4283ef5350bd59c690cf0cd8d669
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-08-23 19:06:46 +0200

    update steam installer

--- install_steam.sh
+++ install_steam.sh
@@ -13,10 +13,11 @@ echo "running "$SCRIPT_NAME
 
 # todo: check if we need this
 # http://askubuntu.com/questions/588024/steam-install-error-on-14-04-ubuntu-64bit
-# sudo apt-get install libc6:i386 libgl1-mesa-dri-lts-vivid:i386 libgl1-mesa-glx-lts-vivid:i386
+sudo apt-get -y install libc6:i386 libgl1-mesa-dri-lts-vivid:i386 libgl1-mesa-glx-lts-vivid:i386 &>> $SCRIPT_LOGFILE
 
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B05498B7
-echo "deb http://repo.steampowered.com/steam/ precise steam" | sudo tee -a  /etc/apt/sources.list.d/steam.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B05498B7 &>> $SCRIPT_LOGFILE
+echo "deb http://repo.steampowered.com/steam/ precise steam" | sudo tee -a  /etc/apt/sources.list.d/steam.list &>> $SCRIPT_LOGFILE
 sudo apt-get -y update >> $SCRIPT_LOGFILE
 sudo apt-get -y install steam-launcher >> $SCRIPT_LOGFILE
+
