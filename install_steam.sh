73648d737e9146b6c49e260ad3e25dadda242217
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-08-23 20:12:27 +0200

    update steam install

--- install_steam.sh
+++ install_steam.sh
@@ -11,13 +11,15 @@ echo "running "$SCRIPT_NAME
 # http://doc.ubuntu-fr.org/steam
 
 
-# todo: check if we need this
+# todo: check if it works with previous versions of 14.04
 # http://askubuntu.com/questions/588024/steam-install-error-on-14-04-ubuntu-64bit
 sudo apt-get -y install libc6:i386 libgl1-mesa-dri-lts-vivid:i386 libgl1-mesa-glx-lts-vivid:i386 &>> $SCRIPT_LOGFILE
 
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B05498B7 &>> $SCRIPT_LOGFILE
-echo "deb http://repo.steampowered.com/steam/ precise steam" | sudo tee -a  /etc/apt/sources.list.d/steam.list &>> $SCRIPT_LOGFILE
-sudo apt-get -y update >> $SCRIPT_LOGFILE
-sudo apt-get -y install steam-launcher >> $SCRIPT_LOGFILE
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B05498B7 &>> $SCRIPT_LOGFILE
+#echo "deb http://repo.steampowered.com/steam/ precise steam" | sudo tee -a  /etc/apt/sources.list.d/steam.list &>> $SCRIPT_LOGFILE
+#sudo apt-get -y update >> $SCRIPT_LOGFILE
+#sudo apt-get -y install steam-launcher >> $SCRIPT_LOGFILE
+
+sudo apt-get -y install steam >> $SCRIPT_LOGFILE
 
