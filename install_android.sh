145bb557a26a58e35f1c303e523eb4478451a57c
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-08-25 17:27:35 +0200

    add android SDK and NDK to PATH

--- install_android.sh
+++ install_android.sh
@@ -12,13 +12,17 @@ sudo apt-get -y install ubuntu-make &>> $SCRIPT_LOGFILE
 
 if [ ! -d /home/$USER/tools/android/android-ndk ] ; then
 	yes 'a' | umake android android-ndk /home/$USER/tools/android/android-ndk
+	echo 'export PATH="$HOME/tools/android/android-ndk:$PATH"' >> ~/.bashrc
 else
 	echo "  Android NDK already installed"
 fi
 
 if [ ! -d /home/$USER/tools/android/android-studio ] ; then
 	yes 'a' | umake android android-studio /home/$USER/tools/android/android-studio
+	echo 'export PATH="$HOME/Android/Sdk/platform-tools:$PATH"' >> ~/.bashrc
 else
+	echo "  Android SDK already installed"
 	echo "  Android Studio already installed"
+	echo "  !!! Please logout/relogin to be able to use Android SDK tools"
 fi
 
