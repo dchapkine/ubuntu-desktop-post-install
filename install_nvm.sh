31154058dcd0d89d49f441418a1cb50aee84c4c3
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-09-06 22:33:07 +0200

    update nvm installer

--- install_nvm.sh
+++ install_nvm.sh
@@ -10,34 +10,53 @@ echo "running "$SCRIPT_NAME
 
 
 
-# install stable ubuntu packages
-# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
-#sudo apt-get -y install nodejs
-#sudo apt-get -y install npm
+if grep -Fxq '# installed by ubuntu-desktop-post-install/install_nvm.sh #' ~/.bashrc
+then
+	
+	echo "  nvm already installed"
+
+else
 
 
-# install more uptodate packages
-# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
-#curl -sL https://deb.nodesource.com/setup | sudo bash -
-#sudo apt-get update
-#sudo apt-get install nodejs
-#sudo apt-get install build-essential
+	# install stable ubuntu packages
+	# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
+	#sudo apt-get -y install nodejs
+	#sudo apt-get -y install npm
 
 
-# install using node version manager
-# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
-sudo apt-get -y install build-essential libssl-dev &>> $SCRIPT_LOGFILE
-curl -sS https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh &>> $SCRIPT_LOGFILE
-#source ~/.bashrc
+	# install more uptodate packages
+	# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
+	#curl -sL https://deb.nodesource.com/setup | sudo bash -
+	#sudo apt-get update
+	#sudo apt-get install nodejs
+	#sudo apt-get install build-essential
 
-echo "  run 'nvm install stable', (logout & relogin first)"
 
-# exec bash -c "nvm ls-remote; nvm install stable"
+	# install using node version manager
+	# https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
 
-#echo "TODO: PLEASE LOGOUT AND LOGIN AGAIN AFTER INSTALLATION FINISHES"
-#echo "After install, check available version by typing 'nvm ls-remote'"
-#echo "Then install version(s) you need by running, for example: 'nvm install 0.12.7' or use 'nvm install stable' to install latest stable version"
-#echo "Usually, nvm will switch to use the most recently installed version. You can explicitly tell nvm to use the version we just downloaded by typing: 'nvm use 0.12.7'"
-#echo "If you have multiple Node.js versions, you can see what is installed by typing: 'nvm ls'"
 
+	echo '' >> ~/.bashrc
+	echo '#' >> ~/.bashrc
+	echo '# nvm' >> ~/.bashrc
+	echo '#' >> ~/.bashrc
+	echo '# installed by ubuntu-desktop-post-install/install_nvm.sh #' >> ~/.bashrc
+	echo '#' >> ~/.bashrc
+	echo '' >> ~/.bashrc
+
+	sudo apt-get -y install build-essential libssl-dev &>> $SCRIPT_LOGFILE
+	curl -sS https://raw.githubusercontent.com/creationix/nvm/v0.16.1/install.sh | sh &>> $SCRIPT_LOGFILE
+	#source ~/.bashrc
+
+	echo "  run 'nvm install stable', (logout & relogin first)"
+
+	# exec bash -c "nvm ls-remote; nvm install stable"
+
+	#echo "TODO: PLEASE LOGOUT AND LOGIN AGAIN AFTER INSTALLATION FINISHES"
+	#echo "After install, check available version by typing 'nvm ls-remote'"
+	#echo "Then install version(s) you need by running, for example: 'nvm install 0.12.7' or use 'nvm install stable' to install latest stable version"
+	#echo "Usually, nvm will switch to use the most recently installed version. You can explicitly tell nvm to use the version we just downloaded by typing: 'nvm use 0.12.7'"
+	#echo "If you have multiple Node.js versions, you can see what is installed by typing: 'nvm ls'"
+
+fi
 
