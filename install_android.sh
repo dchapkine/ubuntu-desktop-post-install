02a902a014b0fbda8478c2ef089f6395ba9cc74c
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-09-06 22:06:39 +0200

    updated android installer

--- install_android.sh
+++ install_android.sh
@@ -3,26 +3,34 @@
 SCRIPT_NAME=$(basename $BASH_SOURCE)
 SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
 
+
 echo "running "$SCRIPT_NAME
 
 
-sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make &>> $SCRIPT_LOGFILE 
-sudo apt-get -y update &>> $SCRIPT_LOGFILE 
-sudo apt-get -y install ubuntu-make &>> $SCRIPT_LOGFILE 
+if grep -Fxq "# installed by ubuntu-desktop-post-install/install_android.sh #" ~/.bashrc
+then
+	
+	echo "  Android sdk, ndk, studio already installed"
 
-if [ ! -d /home/$USER/tools/android/android-ndk ] ; then
+else
+
+	sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make &>> $SCRIPT_LOGFILE 
+	sudo apt-get -y update &>> $SCRIPT_LOGFILE 
+	sudo apt-get -y install ubuntu-make &>> $SCRIPT_LOGFILE 
+
 	yes 'a' | umake android android-ndk /home/$USER/tools/android/android-ndk
-	echo 'export PATH="$HOME/tools/android/android-ndk:$PATH"' >> ~/.bashrc
-else
-	echo "  Android NDK already installed"
-fi
-
-if [ ! -d /home/$USER/tools/android/android-studio ] ; then
 	yes 'a' | umake android android-studio /home/$USER/tools/android/android-studio
-	echo 'export PATH="$HOME/Android/Sdk/platform-tools:$PATH"' >> ~/.bashrc
-else
-	echo "  Android SDK already installed"
-	echo "  Android Studio already installed"
-	echo "  !!! Please logout/relogin to be able to use Android SDK tools"
-fi
 
+	echo '' >> ~/.bashrc
+	echo '#' >> ~/.bashrc
+	echo '# android sdk, nkd, studio' >> ~/.bashrc
+	echo '#' >> ~/.bashrc
+	echo '# installed by ubuntu-desktop-post-install/install_android.sh #' >> ~/.bashrc
+	echo '#' >> ~/.bashrc
+	echo 'export PATH="$HOME/tools/android/android-ndk:$PATH"' >> ~/.bashrc
+	echo 'export PATH="$HOME/Android/Sdk/platform-tools:$PATH"' >> ~/.bashrc
+	echo '' >> ~/.bashrc
+
+	echo "  !!! Please logout/relogin to be able to use Android SDK tools"
+
+fi
