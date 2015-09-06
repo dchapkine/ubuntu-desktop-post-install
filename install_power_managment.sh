38eb3cb17521b24d98b9398a14df6f5a89c55462
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-09-06 23:32:56 +0200

    ...

--- install_power_managment.sh
+++ install_power_managment.sh
@@ -11,9 +11,6 @@ echo "running "$SCRIPT_NAME
 # https://help.ubuntu.com/community/PowerManagement/ReducedPower
 # http://askubuntu.com/questions/285434/is-there-a-power-saving-application-similar-to-jupiter
 
+
 sudo apt-get -y install powertop &>> $SCRIPT_LOGFILE
 sudo apt-get -y install cpufrequtils &>> $SCRIPT_LOGFILE
-
-
-
-
