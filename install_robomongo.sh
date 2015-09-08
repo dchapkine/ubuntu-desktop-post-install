57409aa3dba5be453fd28f7c4a8657b2c2cf7ef8
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-09-08 22:56:47 +0200

    add robomongo installer

---
+++ install_robomongo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
+
+echo "running "$SCRIPT_NAME
+
+
+# tested: works on ubuntu 14.04.3 gnome
+
+sudo apt-get install -y libxcb-render-util0 libxcb-xkb1 libxkbcommon-x11-0
+
+
+URL='http://robomongo.org/files/linux/robomongo-0.8.5-x86_64.deb'
+FILE=`mktemp`
+wget "$URL" -qO $FILE
+sudo dpkg -i $FILE
+rm $FILE
+
+
+
