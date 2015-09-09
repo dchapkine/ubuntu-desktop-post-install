45ca678d2e0994dc504d12899f671e6f8b6cc39f
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-09-09 06:02:38 +0200

    update ubuntu sdk installer

--- install_ubuntu_sdk.sh
+++ install_ubuntu_sdk.sh
@@ -7,8 +7,21 @@ echo "running "$SCRIPT_NAME
 
 
 
-sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa
-sudo apt -y update && sudo apt -y install ubuntu-sdk
-sudo apt -y update && sudo apt -y dist-upgrade
+
+
+# todo: find better check
+if hash ubuntu-sdk 2>/dev/null; then
+	echo "  ubuntu sdk already installed"
+else
+	echo "  ubuntu sdk not installed"
+
+	sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa
+	sudo apt -y update && sudo apt -y install ubuntu-sdk
+	sudo apt -y update && sudo apt -y dist-upgrade
+
+
+fi
+
+
 
 
