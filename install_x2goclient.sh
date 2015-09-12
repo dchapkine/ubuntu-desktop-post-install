077bad51877bda6d2ff899b9fc32106fc2d3bda4
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-09-12 21:02:47 +0200

    add x2go client installer

---
+++ install_x2goclient.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
+SCRIPT_ENVFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".env"
+
+
+echo "running "$SCRIPT_NAME
+
+
+sudo apt-get -y install x2goclient &>> $SCRIPT_LOGFILE
+
+
+
+
