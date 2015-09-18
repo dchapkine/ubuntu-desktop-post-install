72b008aab671aa522d3847fece1d2a452b505d3e
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2015-09-18 03:34:11 +0200

    add atom editor installer

---
+++ install_atom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
+
+echo "running "$SCRIPT_NAME
+
+
+# tested: works on ubuntu 14.04.3 gnome
+
+
+URL='https://atom.io/download/deb'
+FILE=`mktemp`
+wget "$URL" -qO $FILE
+sudo dpkg -i $FILE
+rm $FILE
+
+
