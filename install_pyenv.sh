f1dd0d94a07ec2494d794244da1d369f218a983c
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-01-24 17:53:56 +0100

    pyenv installer

---
+++ install_pyenv.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
+SCRIPT_ENVFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".env"
+
+echo "running "$SCRIPT_NAME
+
+
+# tested: works on ubuntu 14.04.3 gnome
+
+if hash pyenv 2>/dev/null; then
+	echo "  pyenv already installed"
+else
+	echo "  pyenv not installed => installing pyenv"
+
+	git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+	git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+fi
+
+
+if grep -Fxq '# installed by ubuntu-desktop-post-install/install_pyenv.sh #' ~/.bashrc
+then
+	
+	echo "  pyenv already added to bashrc"
+
+else
+
+	echo '' >> ~/.bashrc
+	echo '#' >> ~/.bashrc
+	echo '# pyenv' >> ~/.bashrc
+	echo '#' >> ~/.bashrc
+	echo '# installed by ubuntu-desktop-post-install/install_pyenv.sh #' >> ~/.bashrc
+	echo '#' >> ~/.bashrc
+	echo '' >> ~/.bashrc
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+	echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+	echo 'export PATH="$HOME/.pyenv/shims:$PATH"' >> ~/.bashrc
+	echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+fi
+
+
+echo "  !!! PLEASE LOGOUT AND LOGIN AGAIN AFTER INSTALLATION FINISHES"
+echo "      cool tricks"
+echo "      pyenv install --list"
+echo "      pyenv install 2.7.9"
+echo "		pyenv rehash"
+echo "      pyenv global 2.7.9"
+
+
