e00f36373b0f3818fa2911a8deb1df51b5dc065e
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-03-12 19:42:40 +0100

    gulp global utils

---
+++ install_gulp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
+
+echo "running "$SCRIPT_NAME
+
+# tested: works on ubuntu 14.04.3 gnome
+
+
+if hash gulp 2>/dev/null; then
+	echo "  gulp already installed"
+else
+	echo "  gulp not installed, installing it now"
+
+	npm install --global gulp-cli
+
+fi
