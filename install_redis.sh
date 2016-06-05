e8b35bbad8681b93fc4043e55ee96459a010a195
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-06-05 21:48:03 +0200

    redis installer

---
+++ install_redis.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
+SCRIPT_ENVFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".env"
+
+echo "running "$SCRIPT_NAME
+
+
+# tested: works on ubuntu 14.04.3 gnome
+
+# checking redis
+if hash redis 2>/dev/null; then
+	echo "  checking redis => ok"
+else
+	echo "  checking redis => redis not installed, installing it now"
+
+
+	sudo add-apt-repository -y ppa:chris-lea/redis-server
+	sudo apt-get -y update
+	sudo apt-get -y install redis-server
+
+
+fi
+
+
+
+
