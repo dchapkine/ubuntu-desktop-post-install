91267318d732bc33236202dece1b7c29adf8fcb1
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-06-05 22:42:09 +0200

    add node check in gulp installer

--- install_gulp.sh
+++ install_gulp.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
+# tested on ubuntu 16.04 (gnome)
+# tested on ubuntu 14.04.3 (gnome)
+
 SCRIPT_NAME=$(basename $BASH_SOURCE)
 SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
 mkdir -p ./logs && chmod 755 ./logs
 
 echo "running "$SCRIPT_NAME
 
-# tested: works on ubuntu 14.04.3 gnome
 
+# checking nodejs
+if hash node 2>/dev/null; then
+	echo "  checking node => ok"
+else
+	echo "  checking node => node not installed, please install it and retry"
+	exit;
+fi
 
+# checking npm
+if hash npm 2>/dev/null; then
+	echo "  checking npm => ok"
+else
+	echo "  checking npm => npm not installed, please install it and retry"
+	exit;
+fi
+
+# checking gulp
 if hash gulp 2>/dev/null; then
 	echo "  gulp already installed"
 else
