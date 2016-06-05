81fd0d19eae378085c53324b54230927c23fe607
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-06-05 20:45:26 +0200

    nvidia installer

--- install_gpu_nvidia.sh
+++ install_gpu_nvidia.sh
@@ -8,31 +8,16 @@ echo "running "$SCRIPT_NAME
 # tested: works on ubuntu 16.04 gnome
 
 # add nvidia ppa
-grep graphics-drivers/ppa /etc/apt/sources.list /etc/apt/sources.list.d/*
-
-if [ "$?" != "0" ] ; then
-	echo "  nvidia ppa already added"
-else
-	echo "  nvidia ppa not added yet"
-
-	sudo apt-get -y purge nvidia-*
-	sudo add-apt-repository ppa:graphics-drivers/ppa
-	sudo apt-get -y update
-fi
+sudo apt-get -y purge nvidia-*
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt-get -y update
 
 # install nvidia drivers and nvidia prime
-dpkg-query -l "nvidia-prime" | grep -q ^.i
+sudo apt-get -y install nvidia-367 nvidia-prime
 
-if [ "$?" != "0" ] ; then
-	echo "  nvidia-prime package already installed"
-else
-	echo "  nvidia-prime package not installed"
 
-	sudo apt-get -y install nvidia-367 nvidia-prime
-	
-	#sudo service gdm3 restart
+echo "	you should reboot now"
 
-fi
 
 
 
