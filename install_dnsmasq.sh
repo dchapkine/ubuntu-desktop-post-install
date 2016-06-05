c1db1566f449b78e983c533669d4b9b021f51752
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-06-05 21:49:16 +0200

    dnsmasq

---
+++ install_dnsmasq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
+
+echo "running "$SCRIPT_NAME
+
+# tested: works on ubuntu 14.04.3 gnome
+
+
+if hash dnsmasq 2>/dev/null; then
+	echo "  dnsmasq already installed"
+else
+	echo "  dnsmasq not installed, installing it now"
+
+	sudo apt-get -y install dnsmasq
+	echo "	now edit this file: sudo emacs /etc/dnsmasq.conf"
+	echo "	don't forget to restart"
+
+fi
+
