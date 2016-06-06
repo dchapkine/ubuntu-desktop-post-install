918647434e3e7df3195ed61dd2884e62aface768
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-06-06 06:52:28 +0200

    update vbox installer

--- install_virtualbox.sh
+++ install_virtualbox.sh
@@ -8,12 +8,40 @@ echo "running "$SCRIPT_NAME
 
 
 
-# tested: works on ubuntu 14.04.3 gnome
+. /etc/lsb-release
+echo $DISTRIB_DESCRIPTION
+
+
+# ubuntu 14.04.3 gnome
 # http://doc.ubuntu-fr.org/virtualbox
+if [ "$DISTRIB_RELEASE" = "14.04" ]; then
 
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -  &>> $SCRIPT_LOGFILE
-echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list &>> $SCRIPT_LOGFILE
-sudo apt-get update &>> $SCRIPT_LOGFILE
-sudo apt-get -y install virtualbox-5.0 &>> $SCRIPT_LOGFILE
-sudo usermod -G vboxusers -a $USER &>> $SCRIPT_LOGFILE
+	echo "running UBUNTU 14.04 version"
+	
+	wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -  &>> $SCRIPT_LOGFILE
+	echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list &>> $SCRIPT_LOGFILE
+	sudo apt-get update &>> $SCRIPT_LOGFILE
+	sudo apt-get -y install virtualbox-5.0 &>> $SCRIPT_LOGFILE
+	sudo usermod -G vboxusers -a $USER &>> $SCRIPT_LOGFILE
 
+# ubuntu 16.04 gnome
+elif [ "$DISTRIB_RELEASE" = "16.04" ]; then
+
+	echo "running UBUNTU 16.04 version"
+
+	wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc -O- | sudo apt-key add -  &>> $SCRIPT_LOGFILE
+	echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list &>> $SCRIPT_LOGFILE
+	sudo apt-get update &>> $SCRIPT_LOGFILE
+	sudo apt-get -y install virtualbox-5.0 &>> $SCRIPT_LOGFILE
+	sudo usermod -G vboxusers -a $USER &>> $SCRIPT_LOGFILE
+	
+
+else
+	echo "no compatible installer found for your distribution"
+fi
+
+
+
+
+
+https://www.virtualbox.org/download/oracle_vbox_2016.asc
