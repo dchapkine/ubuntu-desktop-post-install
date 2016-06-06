954b23c94d78d97e892215e5bec00edf6f8b6e07
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-06-06 07:31:01 +0200

    added exfat support

---
+++ install_exfat_support.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# tested on ubuntu 16.04 (gnome)
+# tested on ubuntu 14.04.3 (gnome)
+
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
+mkdir -p ./logs && chmod 755 ./logs
+
+echo "running "$SCRIPT_NAME
+
+
+sudo apt-get -y install exfat-fuse exfat-utils
+
+
+
