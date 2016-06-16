81a9f7409bfcaff1474f6d714e1fb64a53b143c2
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-06-17 00:25:18 +0200

    added tarsnap installer

---
+++ install_tarsnap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# tested on ubuntu 14.04.3 (gnome)
+
+SCRIPT_NAME=$(basename $BASH_SOURCE)
+SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
+mkdir -p ./logs && chmod 755 ./logs
+
+echo "running "$SCRIPT_NAME
+
+
+# checking tarsnap
+if hash tarsnap 2>/dev/null; then
+	echo "  tarsnap already installed"
+else
+	echo "  tarsnap not installed, installing it now"
+
+	# install dependencies
+	sudo apt-get -y install build-essential ext2fs-dev zlib1g-dev libssl-dev 
+
+	# get latest sources
+	version="1.0.37";
+	mkdir -p ~/.tarsnap/installer/$version
+	cd ~/.tarsnap/installer/$version
+	wget https://www.tarsnap.com/download/tarsnap-autoconf-$version.tgz
+	wget https://www.tarsnap.com/download/tarsnap-sigs-$version.asc
+	
+	# display signatures
+	gpg --decrypt tarsnap-sigs-$version.asc
+	shasum -a 256 tarsnap-autoconf-$version.tgz
+
+	# extract & compile
+	tar -xzf tarsnap-autoconf-$version.tgz
+	cd tarsnap-autoconf-$version/
+	./configure
+	make all
+	sudo make install
+
+	# https://www.tarsnap.com/gettingstarted.html
+	sudo mv /usr/local/etc/tarsnap.conf.sample /usr/local/etc/tarsnap.conf
+
+	echo ""
+	echo "What to do next ?"
+	echo "  register this machine"
+	echo "  sudo tarsnap-keygen --keyfile /root/tarsnap.key --user me@example.com --machine mybox"
+
+fi
