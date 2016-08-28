699767c21df49fde0041edb1da480f418261c99c
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-08-28 23:27:49 +0200

    ST3 installer update for ubuntu 16.04

--- install_sublimetext3.sh
+++ install_sublimetext3.sh
@@ -2,13 +2,13 @@
 
 SCRIPT_NAME=$(basename $BASH_SOURCE)
 SCRIPT_LOGFILE="./logs/"$(basename -s .sh $BASH_SOURCE)".log"
-mkdir -p ./logs && chmod 755 ./logs
 
 echo "running "$SCRIPT_NAME
 
 
 
 # tested: works on ubuntu 14.04.3 gnome
+# tested: works on ubuntu 16.04   gnome
 # http://askubuntu.com/questions/172698/how-do-i-install-sublime-text-2-3
 
 
