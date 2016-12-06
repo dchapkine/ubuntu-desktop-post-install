8b841834afdfcf45d360a9cdf1470c539b6983f1
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-12-06 15:07:23 +0100

    up docker

--- install_docker.sh
+++ install_docker.sh
@@ -9,6 +9,7 @@ echo "running "$SCRIPT_NAME
 
 
 # tested: works on ubuntu 14.04.3 gnome
+# tested: works on ubuntu 16.04 gnome
 
 # this installs docker and makes it runnable without sudo
 # because of that you will need to logout/login or reboot your machine after install
@@ -16,25 +17,19 @@ echo "running "$SCRIPT_NAME
 #https://docs.docker.com/installation/ubuntulinux/
 
 
-####### OLD ########
-#sudo apt-get -y install curl &>> $SCRIPT_LOGFILE 
-#curl -sSL https://get.docker.com/ | sh &>> $SCRIPT_LOGFILE
-#sudo usermod -aG docker $USER
-#sudo service docker start &>> $SCRIPT_LOGFILE
-####### OLD ########
-
-
-
-
-
+. /etc/lsb-release
+echo $DISTRIB_DESCRIPTION
 
 
 if hash docker 2>/dev/null; then
-	echo "  docker already installed"
+    echo "  docker already installed"
 else
-	echo "  docker not installed => installing docker"
+    echo "  docker not installed => installing docker"
+
+    if [ "$DISTRIB_RELEASE" = "14.04" ]; then
 
 	
+	sudo apt-get update
 	sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 	sudo echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
 	sudo apt-get -y update
@@ -42,10 +37,23 @@ else
 	sudo apt-cache policy docker-engine
 	sudo apt-get -y install docker-engine
 	sudo usermod -aG docker $USER
+	
 
+    elif [ "$DISTRIB_RELEASE" = "16.04" ]; then
+	
+	sudo apt-get update
+	sudo apt-get install apt-transport-https ca-certificates
+	sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+	echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
+	sudo apt-get -y update
+	apt-cache policy docker-engine
+	sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
+	sudo apt-get -y install docker-engine
+	sudo usermod -aG docker $USER
+	sudo service docker start
+
+    fi
 fi
-
-
 
 
 
