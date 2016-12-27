7f0c56ac3bca70d0bc68f432481567614afd1f2c
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2016-12-27 04:56:53 +0100

    phantomjs update

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -8,30 +8,39 @@ mkdir -p ./logs && chmod 755 ./logs
 
 echo "running "$SCRIPT_NAME
 
-if hash phantomjs 2>/dev/null; then
-	echo "  phantomjs already installed"
+#PJS="phantomjs-2.1.1-linux-x86_64"
+PJS="phantomjs-1.9.8-linux-x86_64"
+
+if [ "$1" == "rm" ]; then
+
+    echo "  removing phantomjs"
+    sudo rm -f /usr/local/bin/phantomjs
+    sudo rm -rf /usr/local/share/$PJS
+
 else
-	echo "  phantomjs not installed"
+	if hash phantomjs 2>/dev/null; then
+		echo "  phantomjs already installed"
+	else
+		echo "  phantomjs not installed"
 
 
-	sudo apt-get update
-	sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
-	sudo apt-get install libfreetype6 libfreetype6-dev -y
-	sudo apt-get install libfontconfig1 libfontconfig1-dev -y
+		sudo apt-get update
+		sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
+		sudo apt-get install libfreetype6 libfreetype6-dev -y
+		sudo apt-get install libfontconfig1 libfontconfig1-dev -y
 
 
-	PJS="phantomjs-2.1.1-linux-x86_64"
-	rm -rf ~/.phantomjs
-	mkdir -p ~/.phantomjs
-	cd ~/.phantomjs
-	wget https://bitbucket.org/ariya/phantomjs/downloads/$PJS.tar.bz2
-	tar xvjf $PJS.tar.bz2
+		rm -rf ~/.phantomjs
+		mkdir -p ~/.phantomjs
+		cd ~/.phantomjs
+		wget https://bitbucket.org/ariya/phantomjs/downloads/$PJS.tar.bz2
+		tar xvjf $PJS.tar.bz2
 
-	sudo mv $PJS /usr/local/share
-	sudo ln -sf /usr/local/share/$PJS/bin/phantomjs /usr/local/bin
+		sudo mv $PJS /usr/local/share
+		sudo ln -sf /usr/local/share/$PJS/bin/phantomjs /usr/local/bin
 
-	rm -rf ~/.phantomjs
+		rm -rf ~/.phantomjs
 
-	phantomjs --version
+		phantomjs --version
+	fi
 fi
-
